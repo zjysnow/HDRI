36c922c0fdd56dc62cb952d10a0e5d01e2b9a772
Albert Zou <zjysnow@outlook.com>
2023-03-01 09:23:57 +0800

    add wihte point for YUV

--- color_space/yuv.py
+++ color_space/yuv.py
@@ -17,8 +17,8 @@ def getMatrixYPbPr2RGB(Yr, Yb):
         [1, 2-2*Yb, 0]
     ])
 
-def getMatrixRGB2YUV(gamut, is_narrow=False, weight_bits=8, offset_bits=8):
-    Yr, _, Yb = getMatrixRGB2XYZ(gamut)[1]
+def getMatrixRGB2YUV(gamut, white_point= np.array([0.31271, 0.32902]), is_narrow=False, weight_bits=8, offset_bits=8):
+    Yr, _, Yb = getMatrixRGB2XYZ(gamut, white_point)[1]
     M = getMatrixRGB2YPbPr(Yr, Yb)
     
     scale = (np.array([[219],[224],[224]])<<(offset_bits-8)) / (2**offset_bits-1) if is_narrow else np.ones((3,1))
@@ -27,8 +27,8 @@ def getMatrixRGB2YUV(gamut, is_narrow=False, weight_bits=8, offset_bits=8):
     return np.round((2**weight_bits) * scale * M), offset << (offset_bits - 8)
 
 
-def getMatrixYUV2RGB(gamut, is_narrow=False, weight_bits=8, offset_bits=8):
-    Yr, _, Yb = getMatrixRGB2XYZ(gamut)[1]
+def getMatrixYUV2RGB(gamut, white_point= np.array([0.31271, 0.32902]), is_narrow=False, weight_bits=8, offset_bits=8):
+    Yr, _, Yb = getMatrixRGB2XYZ(gamut, white_point)[1]
     M = getMatrixYPbPr2RGB(Yr, Yb)
 
     scale = (2**offset_bits-1)/(np.array([219,224,224])<<(offset_bits-8)) if is_narrow else np.ones((3,1))
