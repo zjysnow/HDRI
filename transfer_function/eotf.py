078c2f9a2e964853b5993d73347626f97d4efd49
Albert Zou <zjysnow@outlook.com>
2023-03-31 11:00:39 +0800

    add transfer function

--- transfer_function/eotf.py
+++ transfer_function/eotf.py
@@ -7,3 +7,19 @@ def PQ(x):
     c2 = 2413 / 4096 * 32
     c3 = 2392 / 4096 * 32 
     return (np.maximum((x**(1/m2))-c1, 0) / (c2 - c3 * (x**(1/m2)))) ** (1/m1)
+
+def sRGB(x):
+    return np.piecewise(
+        x, [x < 0.04045], [
+        lambda x: x / 12.92,
+        lambda x: ((x + 0.055) / 1.055)**2.4
+        ]
+    )
+
+def BT709(x):
+    return np.piecewise(
+        x, [x < 0.08125], [
+        lambda x: x / 4.5,
+        lambda x: ((x+0.099)/1.099)**(1/0.45)
+        ]
+    )
--- transfer_function/oetf.py
+++ transfer_function/oetf.py
@@ -1,1 +1,25 @@
 import numpy as np
+
+def PQ(x):
+    m1 = 2610 / 16384
+    m2 = 2523 / 4096 * 128
+    c1 = 3424 / 4096
+    c2 = 2413 / 4096 * 32
+    c3 = 2392 / 4096 * 32 
+    return ((c1+c2*(x**m1))/(1+c3*(x**m1)))**m2
+
+def sRGB(x):
+    return np.piecewise(
+        x, [x < 0.00304], [ 
+        lambda x: 12.92*x, # x < 0.00304 
+        lambda x: 1.055 * x**(1/2.4) - 0.055 # else
+        ]
+    )
+
+def BT709(x):
+    return np.piecewise(
+        x, [x < 0.018], [
+        lambda x: 4.5*x,
+        lambda x: 1.099*(x**0.45)-0.099
+        ]
+    )
