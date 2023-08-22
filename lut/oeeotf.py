a69bccf0338e19efd1276b44881018ba7caa3ca1
albert <zjysnow@outlook.com>
2023-08-22 13:40:08 +0800

    add lut oetf and ootf

--- lut/oeeotf.py
+++ lut/oeeotf.py
@@ -40,8 +40,85 @@ def lutEOTF(x, lut1, lut2, lut_bit = 24, step_bit1 = 4, step_bit2 = 6, bvalue = 
     y2 = interp_val2 >> step_bit2
     y2 = np.minimum(y2, (1<<lut_bit)-1)
     
-    y = np.zeros_like(x)
+    y = np.zeros_like(x, dtype=np.uint64)
     y[x<=bvalue] = y1
     y[x>bvalue] = y2
 
     return y
+
+def getLutIndex(x, lut1, lut2, bvalue):
+    return np.piecewise(x, [x >= bvalue], [
+        lambda x: [np.sum(lut2<x2) for x2 in x],
+        lambda x: [np.sum(lut1<x1) for x1 in x]
+    ])
+
+# def getLutIndex(x, lut):
+#     return np.array([np.sum(lut<x1) for x1 in x])
+
+def lutOETF(x, lut1, lut2, lut_bit = 24, step_bit1 = 4, step_bit2 = 6):
+
+    bvalue = lut1[-1]
+    index = getLutIndex(x, lut1, lut2, lut1[-1])
+
+    lut_min = np.piecewise(index, [(x<bvalue)&(index==0), (x<bvalue)&(index>9), (x<bvalue)&(index<=9)&(index>0), 
+                                   (x>=bvalue)&(index==0), (x>=bvalue)&(index>0)], [
+        lambda x: 0,
+        lambda x: lut1[-1],
+        lambda x: lut1[x-1],
+        lambda x: lut1[-1],
+        lambda x: lut2[x-1]
+    ])
+
+    lut_max = np.piecewise(index, [(x<bvalue)&(index>8), (x<bvalue)&(index<=8), 
+                                   (x>=bvalue)&(index>=62), (x>=bvalue)&(index<62)], [
+        lambda x: (1<<lut_bit)-1,
+        lambda x: lut1[x],
+        lambda x: (1<<lut_bit)-1,
+        lambda x: lut2[x]
+    ])
+
+    lut_base = np.piecewise(index, [(x<bvalue)&(index==0), (x<bvalue)&(index>0), 
+                                    (x>=bvalue)&(index>62), (x>=bvalue)&(index<62)], [
+        lambda x: 0,
+        lambda x: (x - 1) << 4,
+        lambda x: 4095,
+        lambda x: (x + 2) << 6
+    ])
+
+    step = lut_max - lut_min
+    resi = x - lut_min
+    resi[x<bvalue] <<= (step_bit1 + 1)
+    resi[x>=bvalue] <<= (step_bit2 + 1)
+    
+    resi_carry = resi + step - 1
+    resi_carry[step==0] = 0
+
+    level = np.zeros_like(x)
+    level[step!=0] = np.minimum(resi_carry[step!=0] / step[step!=0], 128)
+
+    inc = level >> 1
+    inc[(x<bvalue)&(level>31)] = 16
+    
+    y = np.minimum(lut_base + inc, 4095)
+    return y
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    lut1, lut2 = getLut2(lambda x: x**2.2)
+
+    x = np.array(range(4096), dtype=np.uint64)
+    y = lutEOTF(x, lut1, lut2)
+
+    v = lutOETF(y, lut1, lut2)
+
+    # plt.plot(x/4095, x/4095, "--")
+    # plt.plot(x/4095., y/16777215.)
+    plt.figure()
+    plt.plot(x, v)
+
+    # x = lutOETF(y, lut1, lut2)
+    # plt.plot(x/4095, y)
+
+    plt.show()
+    
--- lut/ootf.py
+++ lut/ootf.py
@@ -1,7 +1,26 @@
 import numpy as np
 
-def lutOOTF(x, lut, lut_bit = 16):
-    pass
+def interpolate(x, lut, input_bit = 24):
+    bit = (input_bit - np.log2(lut.shape[0] - 1)).astype(np.uint64)
+    print(bit)
+    idx = x >> bit
+    x0 = idx << bit
+    y0 = lut[idx]
+    y1 = lut[idx+1]
+    gain = (((y1-y0)*(x-x0)).astype(np.uint64) >> bit) + y0
+    return gain
 
-def lutOOTF(x, lut1, lut2, lut_bit = 16):
-    pass
+def lutOOTF(x, gain_lut, lut_bit = 16, input_bit = 24):
+    gain = interpolate(x, gain_lut, input_bit)
+    rounding = 1<<(lut_bit-1)
+    print(gain)
+    y = (x * gain + rounding).astype(np.uint64) >> lut_bit
+    y = np.clip(y, 0, (1<<input_bit)-1)
+    return y
+
+if __name__ == "__main__":
+    lut = (np.array(range(257))/256*((1<<16)-1)).astype(np.uint64)
+    print(lut)
+    x = np.round(np.random.rand(3,3) * ((1<<24)-1)).astype(np.uint64)
+    y = lutOOTF(x, lut)
+    print(y)
